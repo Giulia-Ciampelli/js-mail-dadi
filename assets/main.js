ecf1a4569c1102cb7bd70e5c50b153e6a86b8c00
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-03 14:33:23 +0200

    docs: start first problem

--- assets/main.js
+++ assets/main.js
@@ -2,5 +2,22 @@ console.log('im alive');
 
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// creazione array (lista di chi può accedere)*
+let users = [
+    'sunnyday123@example.com',
+    'bluebird.music@mail.com',
+    'techguy88@domain.com',
+    'foodie.adventure@service.org',
+    'wanderlust.2024@mail.net',
+    'booklover99@example.co',
+    'artfanatic@email.com',
+    'gamerpro2023@site.com',
+    'naturephotographer@web.com',
+    'coffeeenthusiast@domain.org',
+];
+
+// creazione if statement per verificare se l'user sia nella lista
+
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
